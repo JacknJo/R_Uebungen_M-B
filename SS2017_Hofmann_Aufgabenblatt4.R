61f2a36516c34f23e38816dd46e05b94eb16d494
Florian Hofmann <florian.hofmann10@gmail.com>
2017-06-14 10:37:34 +0200

    Updated content of 14th June 17

--- SS2017_Hofmann_Aufgabenblatt4.R
+++ SS2017_Hofmann_Aufgabenblatt4.R
@@ -1,7 +1,37 @@
 # SS2017_Hofmann_Aufgabenblatt4
-## Übung7
-## 02. Mai 2017
+## Übung8
+## 24. Mai 2017
 ### open pdf document.
 #pdf("SS2017_Hofmann_Aufgabenblatt4.pdf")
 
-# Auswertung der Formel aus Aufgabenteil (a)
+# b) Exakte Berechnung der Binomialverteilung zum Vergleich der Normalapproximation aus a)
+alpha = 0.05
+n = 180
+p0 = 1/6
+
+### kritischer Wert
+# xc = wqrt(n*p0 * (1-p0))
+
+### pWert
+xstar = 44
+z = (xstar - n* p0 - 0.5)/sqrt(n*p0*(1-p0))
+pW = 1 - pnorm(z)
+pW
+
+# binom.test()
+# Binomialtest in R
+# xc = # kritischer Wert!
+# 1-pbinom(xc,    n, p0)
+# 1-pbinom(xc -1, n, p0) # letzter Wert zum Niveau alpha.
+# 1-pbinom(xc -2, n, p0)
+
+### pWert
+x_star = 44;
+pW = 1 - pbinom(x_star -1, n, p0)
+pW
+
+### mit binom.test
+binom.test(x_star -1, n, p0, alternative="greater")
+
+### Bestimmung mittels Konfidenzintervall
+### Überprüfen, ob behaupteter Wert unter H0 postuliert im Konfidenzintervall liegt.
--- SS2017_Vorlesung_UebungenInR5.R
+++ SS2017_Vorlesung_UebungenInR5.R
@@ -34,3 +34,72 @@ fisher.test(tafel)
 ### Hinweis:
 ### Odds ratio wird anders geschätzt, als manuell herleitbar.
 ### Hier gibt es einen Unterschied zwischen R-Schätzung und händischer.
+
+
+## Chi Quadrat Test in R
+
+#### chisq.test(x, y = NULL, correct = TRUE, p, rescale.p = FALSE)
+#### x matrix der 2x2 Felder Tafel
+#### p Vektor mit den W-keiten für einzelne Kategorien unter H0
+#### correct = TRUE führt bei den Vierfeldertafeln eine Stetigkeitskorrektur durch
+#### angehängtes $expected gibt die unter H0 erwarteten Häufigkeiten aus.
+
+
+tafel = matrix(c(5, 5, 6, 24), 2,2, byrow = TRUE)
+dimnames(tafel) = list(c("hoch", "niedrig"), c("7-12", "13-18"))
+
+tafel
+
+chisq.test(tafel, correct=FALSE)
+chisq.test(tafel)
+chisq.test(tafel)$expected
+
+fisher.test(tafel)
+#### Hinweis: Odds ratio wieder etwas niedriger als manuelle Schätzung.
+
+## Freeman-Halton-Test in R
+
+#### fisher.test(x, alternative="two.sided")
+#### x: betrachtete kx2 Tafel in Form einer Matrix.
+
+### Berechnung zu Fuß
+#### Berechnung der Wahrscheinlichkeit p in abhängigkeit von beiden Freiheitsgraden n1, n2
+#### return = ptafel;
+
+#### Frage: Welche der 18 möglichen Tafeln hat eine Wahrscheinlichkeit <= 2.1
+#### -> 9stk
+#### Summe = 8.82%
+
+### Mit R implementierten Test
+tafel = matrix(c(0,6,3,5,2,1), 3, 2, byrow = TRUE)
+dimnames(tafel) = list(c("gut", "mäßig", "unbefriedigend"), c("Zwang", "Angst"))
+tafel
+
+fisher.test(tafel)
+# auch transponierte tafel kann genommen werden
+fisher.test(t(tafel))
+
+## Berechnung der Trendalternativhypothese
+#komb sind alle tafeln, die man betrachten muss = kombination aller beiden Freiheitsgraden
+# trend = 10 - komb[,1]+komb[,2]
+# trend
+# which(trend >= 12)
+
+# komb[which(trend) >= 12),]
+
+### ausgabe aller extremerer tafeln (Ausgangstafel noch enthalten)
+### Berechnung des p-Wertes durch funktion(n1, n2)
+### pWert = summe der einzelnen pWerte.
+
+
+## kx2 Felder-Chi-Quadrat-Test in R
+### chisq.test(x, y=NULL, correct=TRUE, rescale.p=FALSE)
+#### $expected: unter H0 erwartete Häufigkeiten
+#### $resuduals: einzelne Summanden der Teststatistik
+tafel = matrix(c(30,25,60,55,61,93), 3, 2, byrow = TRUE)
+dimnames(tafel) = list(c("Hebephrene", "Katakone", "Paranoide"), c("m", "w"))
+tafel
+
+chisq.test(tafel)
+round(chisq.test(tafel)$expected, 1)
+round((chisq.test(tafel)$residuals)^2, 2)
